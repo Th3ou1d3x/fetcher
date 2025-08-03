92242d9f931e10511ade74bb411ab36c57aca17f
Th3ou1d3x <avash.kashefi.yl@gmail.com>
2025-08-03 16:59:33 -0700

    Update fetcher.ts

--- fetcher.ts
+++ fetcher.ts
@@ -2,6 +2,6 @@
 namespace fetcher {
     //% block="fetch JSON from %url"
     export function fetch_json(url: string): string {
-        return ""; // Placeholder for the shim
+        return "";
     }
 }
